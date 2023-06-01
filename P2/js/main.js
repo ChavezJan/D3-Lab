c8ba912bc9e5fb2277d3eaa81dc982707dfce776
Jorge Chavez <chavejoa@gmail.com>
2023-05-31 18:19:47 -0600

    P2 - done

--- P2/js/main.js
+++ P2/js/main.js
@@ -1,4 +1,28 @@
 /*
-*    main.js
-*/
+ *    main.js
+ */
 
+var data = [25, 30, 15, 10, 5];
+
+var svg = d3
+  .select("#chart-area")
+  .append("svg")
+  .attr("width", 400)
+  .attr("height", 400);
+
+var rects = svg.selectAll("rect").data(data);
+
+rects
+  .enter()
+  .append("rect")
+  .attr("x", (d, i) => {
+    return i * 100;
+  })
+  .attr("y", (d) => {
+    return -1 * (d - 400);
+  })
+  .attr("width", 40)
+  .attr("height", (d) => {
+    return d;
+  })
+  .attr("fill", "red");
